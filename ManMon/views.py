1de9726b43f3458e266369d00328f3611bcc35e7
FoXIsMaName <s5801012630050@email.kmutnb.ac.th>
2017-03-15 02:13:56 +0700

    add template main.html, add method callMainPage in view.py

--- ManMon/views.py
+++ ManMon/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Account, TypeIncome, TypePayment
 # Create your views here.
 
 def callMainPage(request):
-    return HttpResponse("Hello, this is ManMon app")
+    account_list = Account.objects.order_by('-pub_date')
 
+    #calulation the rest of money
+    remain_money = 0
+    for account in account_list :
+        remain_money += account.money
+
+    account_list = Account.objects.order_by('-pub_date')[:5]
+    return render(request, 'ManMon/main.html',{'remain_money':remain_money,         
+                  'account_list':account_list})
